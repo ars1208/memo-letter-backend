f9f7cf64998ec107debcf4d6914b9e4231db6d18
ars1208 <ara.kaz1208@gmail.com>
2022-08-10 16:21:12 +0900

    fix

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.set('port', (process.env.PORT || 3000));
+
 app.get("/", function (req, res) {
   res.send("Hello world!");
 });
 
-app.listen(3000, () => console.log("Example app listening on port 3000"));
+app.listen(app.get('port'), () => console.log("Example app listening on port " + app.get('port')));
