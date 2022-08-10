9333b001e80af79d3a57d7815b1979df079a0679
ars1208 <ara.kaz1208@gmail.com>
2022-08-10 18:09:08 +0900

    chore: DB情報を追加

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 // DB情報を持ったPoolを生成
-exports.pool = new Pool ({
+const pool = new Pool ({
   host: process.env.ENV_HOST,
   database: process.env.ENV_DATABASE,
   user: process.env.ENV_USER,
