4b084a1a8df0a562ed92f4dde685d1cc12bbd57d
ars1208 <ara.kaz1208@gmail.com>
2022-08-14 23:49:37 +0900

    chore: ログインのロジックをテスト

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
 const app = express();
 
 const pool = require('./db/db');
 
 app.set('port', (process.env.PORT || 3000));
+
+app.post("/login", function(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "https://ars1208.github.io")
+  bcrypt.hash(req.body.password, 10, function(err, hash) {
+    pool.query('SELECT * FROM REGISTERED_USERS WHERE user_name = ArashiKazuki;', function(err, results) {
+      if (err) {
+        throw err
+      }
+      res.setHeader("Access-Control-Allow-Origin", "https://ars1208.github.io")
+      res.status(200).json({
+        data: results.rows
+      });
+    });
+    // var query = pool.query("SELECT * FROM REGISTERED_USERS WHERE user_name = $1;", [req.body.userId]);
+  })
+})
 
 app.get("/", function (req, res) {
   res.setHeader("Access-Control-Allow-Origin", "https://ars1208.github.io")
