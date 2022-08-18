21323c42cedd413105870f6879075e56cd38c4bc
ars1208 <ara.kaz1208@gmail.com>
2022-08-18 23:16:25 +0900

    chore: ログイン機能の実装

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ app.get("/logout", function(req, res) {
 })
 
 app.post("/login", function(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "https://ars1208.github.io")
   bcrypt.hash(req.password, 10, function(err, hash) {
     pool.query(`SELECT * FROM REGISTERED_USERS WHERE user_name="${req.body.userId}";`, async function(err, row) {
       const compared = await bcrypt.compare(req.body.password, row.password);
@@ -34,7 +35,6 @@ app.post("/login", function(req, res) {
         console.log(req.session)
       }
 
-      res.setHeader("Access-Control-Allow-Origin", "https://ars1208.github.io")
       res.redirect("/");
     })
   });
