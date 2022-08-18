ffb195e4d56a5219895ffc4756ac919e3a5e47d1
ars1208 <ara.kaz1208@gmail.com>
2022-08-18 16:10:03 +0900

    chore: ログイン機能の実装

--- index.js
+++ index.js
@@ -2,26 +2,41 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const cors = require("cors")
 const app = express();
+const session = require("express-session");
 
 const pool = require('./db/db');
+const ses_opt = {
+  secret: "secret",
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 60 * 60 * 1000 }
+};
 
-app.use(cors())
+app.use(cors());
+app.use(session(ses_opt));
 
 app.set('port', (process.env.PORT || 3000));
 
-app.get("/login", function(req, res) {
-  // bcrypt.hash(req.body.password, 10, function(err, hash) {
-    pool.query("SELECT * FROM REGISTERED_USERS WHERE user_name='ArashiKazuki';", function(err, results) {
-      if (err) {
-        throw err
+// アカウント関連API
+app.get("/logout", function(req, res) {
+  req.session.login = undefined
+
+  res.redirect("/");
+})
+
+app.post("/login", function(req, res) {
+  bcrypt.hash(req.body.password, 10, function(err, hash) {
+    pool.query(`SELECT * FROM REGISTERED_USERS WHERE user_name="${req.body.userId}";`, async function(err, row) {
+      const compared = await bcrypt.compare(req.body.password, row.password);
+
+      if (compared) {
+        req.session.login = row.userId;
+        console.log(req.session)
       }
-      res.setHeader("Access-Control-Allow-Origin", "https://ars1208.github.io")
-      res.status(200).json({
-        data: results.rows
-      });
-    });
-    // var query = pool.query("SELECT * FROM REGISTERED_USERS WHERE user_name = $1;", [req.body.userId]);
-  // })
+
+      res.redirect("/");
+    })
+  });
 });
 
 app.get("/", function (req, res) {
